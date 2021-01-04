bfb1b4b12c55b6e09555fb4b1ae51d0cec44baf4
Gordon Krieger <gordon.krieger@gmail.com>
2021-01-03 22:07:27 -0500

    init backend and frontend

---
+++ server/index.js
@@ -0,0 +1,24 @@
+"use-strict"
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 8000;
+
+express()
+    .use(morgan("tiny"))
+    .use(express.static("public"))
+    .use(bodyParser.json())
+    .use(express.urlencoded({extended: false}))
+    .use("/", express.static(__dirname + "/"))
+
+
+    .get("*", (req, res) => {
+        res.status(404).json({
+            status: 404,
+            message: "not found"
+        });
+    })
+
+    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
