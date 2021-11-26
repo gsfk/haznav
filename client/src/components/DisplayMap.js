0fdeff799c7881fb3b1e8448650bc89b5a083c64
Gordon Krieger <gordon.krieger@gmail.com>
2021-11-25 22:11:33 -0500

    remove magic numbers

--- client/src/components/DisplayMap.js
+++ client/src/components/DisplayMap.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
+const MONTREAL_LATITUDE = 45.5;
+const MONTREAL_LONGITIUDE = -73.56;
+const ZOOM_LEVEL = 12;
+
 const DisplayMap = ({ height, width }) => {
   const mapRef = useRef(null);
   const [map, setMap] = useState(null);
@@ -17,28 +21,27 @@ const DisplayMap = ({ height, width }) => {
     // Create map instance
     setMap(
       new H.Map(mapRef.current, defaultLayers.vector.normal.map, {
-        center: { lat: 45.5, lng: -73.56 },
-        zoom: 12,
+        center: { lat: MONTREAL_LATITUDE, lng: MONTREAL_LONGITIUDE },
+        zoom: ZOOM_LEVEL,
         pixelRatio: window.devicePixelRatio || 1,
       })
     );
-  
+
     //cleanup on unmount
     return () => setMap(null);
   }, []);
 
   return (
     // Set a height on the map so it will display
-      <MapGraphic ref={mapRef} height={height} width={width}/>
+    <MapGraphic ref={mapRef} height={height} width={width} />
   );
 };
 
-  const MapGraphic = styled.div`
-    width: ${props => props.width}px;
-    height: ${props => props.height }px;
-    border: 5px solid grey;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-  `
-
+const MapGraphic = styled.div`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  border: 5px solid grey;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+`;
 
 export default DisplayMap;
